a202a2ce3e8e07dae158bce4e3296ffc4c735566
MK023 <49072576+MK023@users.noreply.github.com>
2025-09-04 14:13:17 +0200

    Chore: fix security.py to avoid ReDDos Attack

--- security/security.py
+++ security/security.py
@@ -14,6 +14,9 @@ def validate_address(address):
     ip_pattern = r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"
     # Dominio (semplificato)
     domain_pattern = (
-        r"^(?!-)[A-Za-z0-9-]{1,63}(?<!-)\.(?!-)(?:[A-Za-z0-9]{1,63}\.?)+[A-Za-z]{2,}$"
-    )
+    r"^(?!-)[A-Za-z0-9-]{1,63}(?<!-)"
+    r"(?:\.[A-Za-z0-9-]{1,63}(?<!-))*"
+    r"\.[A-Za-z]{2,}$"
+    )   
     return bool(re.match(ip_pattern, address) or re.match(domain_pattern, address))
+
